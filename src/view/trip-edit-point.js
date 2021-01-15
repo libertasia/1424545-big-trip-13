70d2f34d08984a044886fdde75024bd609c0f92c
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-01-15 16:57:54 +0200

    Fixed event type selector

--- src/view/trip-edit-point.js
+++ src/view/trip-edit-point.js
@@ -1,3 +1,4 @@
+import {POINT_TYPES} from "../const.js";
 import dayjs from "dayjs";
 import he from "he";
 import SmartView from "./smart.js";
@@ -92,6 +93,16 @@ const createButtonsTemplate = (isNew, isDisabled, isSaving, isDeleting, destinat
   `;
 };
 
+const createEventTypeItemTemplate = (itemType, currentPointType) => {
+  const isChecked = itemType.toLowerCase() === currentPointType.toLowerCase() ? `checked` : ``;
+  const type = itemType.toLowerCase();
+  return `<div class="event__type-item">
+    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" ${isChecked}>
+    <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${itemType}</label>
+  </div>
+  `;
+};
+
 const createEditPointTemplate = (data, destinations, allOffers) => {
   const {type, destination, price, startTime, endTime, offers, isNew, isDisabled, isSaving, isDeleting} = data;
 
@@ -105,6 +116,7 @@ const createEditPointTemplate = (data, destinations, allOffers) => {
   const offersSectionMarkup = createOffersTemplate(availableOffers, offers, isDisabled);
   const destinationSectionMarkup = createDestinationTemplate(destination);
   const buttonsMarkup = createButtonsTemplate(isNew, isDisabled, isSaving, isDeleting, destination !== null);
+  const eventTypeItemsMarkup = POINT_TYPES.reduce((acc, currentValue) => acc + createEventTypeItemTemplate(currentValue, type), ``);
 
   return `<li class="trip-events__item">
       <form class="event event--edit" action="#" method="post">
@@ -119,56 +131,7 @@ const createEditPointTemplate = (data, destinations, allOffers) => {
             <div class="event__type-list">
               <fieldset class="event__type-group">
                 <legend class="visually-hidden">Event type</legend>
-
-                <div class="event__type-item">
-                  <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-                  <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-                  <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-                  <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-                  <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-transport-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="transport">
-                  <label class="event__type-label  event__type-label--transport" for="event-type-transport-1">Transport</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-                  <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight">
-                  <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
-                  <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
-                  <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
-                  <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
-                </div>
+                ${eventTypeItemsMarkup}
               </fieldset>
             </div>
           </div>
